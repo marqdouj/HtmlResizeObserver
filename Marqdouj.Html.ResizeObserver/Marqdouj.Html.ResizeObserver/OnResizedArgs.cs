2f99e9363f828cfa13322c08ffd8f51346a33131
marqdouj <16510416+marqdouj@users.noreply.github.com>
2025-02-14 07:47:40 -0700

    Created Solution

---
+++ Marqdouj.Html.ResizeObserver/Marqdouj.Html.ResizeObserver/OnResizedArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace Marqdouj.Html.ResizeObserver
+{
+    public class OnResizedArgs(string id, double height, double width)
+    {
+        public string Id { get; } = id;
+        public double Height { get; } = height;
+        public double Width { get; } = width;
+    }
+}
---
+++ Marqdouj.Html.ResizeObserver/Marqdouj.Html.ResizeObserver/ResizeObserverService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.JSInterop;
+
+namespace Marqdouj.Html.ResizeObserver
+{
+    public class ResizeObserverService
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+        private readonly DotNetObjectReference<ResizeObserverService>? dotNetRef;
+
+        public ResizeObserverService(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./_content/Marqdouj.Html.ResizeObserver/ts/js/resizeObserver.js").AsTask());
+            dotNetRef = DotNetObjectReference.Create(this);
+        }
+
+        public async ValueTask Observe(string id)
+        {
+            var module = await moduleTask.Value;
+            await module.InvokeAsync<string>("observe", id, dotNetRef);
+        }
+
+        public async ValueTask UnObserve(string id)
+        {
+            var module = await moduleTask.Value;
+            await module.InvokeAsync<string>("unobserve", id);
+        }
+
+        public event Action<OnResizedArgs>? OnResize;
+
+        [JSInvokable]
+        public void OnResized(string id, double height, double width)
+        {
+            OnResize?.Invoke(new OnResizedArgs(id, height, width));
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            dotNetRef?.Dispose();
+
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
